29d85ea5e4e746af2ab1e4abcfbbef6193b19e35
Scott Mabin <scott@mabez.dev>
2023-07-16 01:13:34 +0100

    Fix up examples

--- examples/write.rs
+++ examples/write.rs
@@ -1,22 +1,16 @@
-// use embedded_io::Write;
-// use fatfs::{FileSystem, FsOptions};
-// use tokio::fs::OpenOptions;
-// use tokio::io::BufStream;
+use anyhow::Context;
+use embedded_io::Write;
+use fatfs::{FileSystem, FsOptions};
+use tokio::fs::OpenOptions;
+use tokio::io::BufStream;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    todo!();
-    // let img_file = match OpenOptions::new().read(true).write(true).open("fat.img").await {
-    //     Ok(file) => file,
-    //     Err(err) => {
-    //         println!("Failed to open image!");
-    //         return Err(err);
-    //     }
-    // };
-    // let buf_stream = BufStream::new(img_file);
-    // let options = FsOptions::new().update_accessed_date(true);
-    // let fs = FileSystem::new(buf_stream, options).await?;
-    // let mut file = fs.root_dir().create_file("hello.txt").await?;
-    // file.write_all(b"Hello World!").await?;
-    // Ok(())
+    let img_file = OpenOptions::new().read(true).write(true).open("fat.img").await.context("Failed to open image!")?;
+    let buf_stream = BufStream::new(img_file);
+    let options = FsOptions::new().update_accessed_date(true);
+    let fs = FileSystem::new(buf_stream, options).await?;
+    let mut file = fs.root_dir().create_file("hello.txt").await?;
+    file.write_all(b"Hello World!").await?;
+    Ok(())
 }
