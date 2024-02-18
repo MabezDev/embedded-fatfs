3dac35ee13e172267a5a75f6fb39b2c8cbc92c5b
Scott Mabin <scott@mabez.dev>
2024-02-18 01:06:51 +0000

    more initialization

--- sdmmc-spi-async/src/lib.rs
+++ sdmmc-spi-async/src/lib.rs
@@ -3,7 +3,8 @@
 use core::fmt::Debug;
 use core::future::Future;
 use embassy_futures::select::{select, Either};
-use sdio_host::common_cmd::*;
+use sdio_host::sd::{CardCapacity, CardStatus, CurrentState, SDStatus, CID, CSD, OCR, SCR, SD};
+use sdio_host::{common_cmd::*, sd_cmd::*};
 
 // MUST be the first module listed
 mod fmt;
@@ -25,6 +26,33 @@ pub const DATA_RES_MASK: u8 = 0x1F;
 /// Write data accepted token
 pub const DATA_RES_ACCEPTED: u8 = 0x05;
 
+#[derive(Clone, Copy, Debug, Default)]
+/// SD Card
+pub struct Card {
+    /// The type of this card
+    pub card_type: CardCapacity,
+    /// Operation Conditions Register
+    pub ocr: OCR<SD>,
+    /// Relative Card Address
+    pub rca: u32,
+    /// Card ID
+    pub cid: CID<SD>,
+    /// Card Specific Data
+    pub csd: CSD<SD>,
+    /// SD CARD Configuration Register
+    pub scr: SCR,
+    /// SD Status
+    pub status: SDStatus,
+}
+
+impl Card {
+    /// Size in bytes
+    pub fn size(&self) -> u64 {
+        // SDHC / SDXC / SDUC
+        u64::from(self.csd.block_count()) * 512
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[non_exhaustive]
@@ -32,6 +60,8 @@ pub enum Error {
     ChipSelect,
     SpiError,
     Timeout,
+    UnsupportedCard,
+    Cmd58Error,
 }
 
 pub struct SpiSdmmc<SPI, CS, D>
@@ -43,6 +73,7 @@ where
     spi: SPI,
     cs: CS,
     delay: D,
+    card: Option<Card>,
 }
 
 impl<SPI, CS, D> SpiSdmmc<SPI, CS, D>
@@ -52,7 +83,12 @@ where
     D: embedded_hal_async::delay::DelayNs + Clone,
 {
     pub fn new(spi: SPI, cs: CS, delay: D) -> Self {
-        Self { spi, cs, delay }
+        Self {
+            spi,
+            cs,
+            delay,
+            card: None,
+        }
     }
 
     pub async fn init(&mut self) -> Result<(), Error> {
@@ -75,6 +111,71 @@ where
         })
         .await??;
 
+        // TODO enable crc
+        // cmd::<R3>(0x3A, 0) // <- custom cmd
+
+        with_timeout(self.delay.clone(), 1000, async {
+            loop {
+                let r = self.cmd(send_if_cond(0x1, 0xAA)).await?;
+                if r == (R1_ILLEGAL_COMMAND | R1_IDLE_STATE) {
+                    return Err(Error::UnsupportedCard);
+                }
+                let mut buffer = [0xFFu8; 4];
+                self.spi
+                    .transfer_in_place(&mut buffer[..])
+                    .await
+                    .map_err(|_| Error::SpiError)?;
+                if buffer[3] == 0xAA {
+                    return Ok(());
+                }
+            }
+        })
+        .await??;
+
+        trace!("Valid card detected!");
+
+        // If we get here we're at least a v2 card
+        let mut card = Card::default();
+
+        // send ACMD41
+        with_timeout(self.delay.clone(), 1000, async {
+            loop {
+                let r = self.acmd(sd_send_op_cond(true, false, true, 0x20)).await?;
+                if r == R1_READY_STATE {
+                    return Ok(());
+                }
+            }
+        })
+        .await??;
+
+        trace!("Reading back OCD register");
+
+        card.card_type = with_timeout(self.delay.clone(), 1000, async {
+            loop {
+                let r = self.cmd(cmd::<R3>(0x3A, 0)).await?;
+                if r != R1_READY_STATE {
+                    return Err(Error::Cmd58Error);
+                }
+                let mut buffer = [0xFFu8; 4];
+                self.spi
+                    .transfer_in_place(&mut buffer[..])
+                    .await
+                    .map_err(|_| Error::SpiError)?;
+                return Ok(if buffer[0] & 0xC0 == 0xC0 {
+                    CardCapacity::HighCapacity
+                } else {
+                    CardCapacity::StandardCapacity
+                });
+            }
+        })
+        .await??;
+
+        trace!("Card initialized: {:?}", card);
+
+        self.card = Some(card);
+
+        self.cs.set_high().map_err(|_| Error::ChipSelect)?;
+        let _ = self.read_byte().await;
         Ok(())
     }
 
@@ -116,6 +217,12 @@ where
         Ok(byte)
     }
 
+    async fn acmd<R: Resp>(&mut self, cmd: Cmd<R>) -> Result<u8, Error> {
+        self.cmd(app_cmd(self.card.map(|c| c.rca).unwrap_or(0) as u16))
+            .await?;
+        self.cmd(cmd).await
+    }
+
     async fn wait_idle(&mut self) -> Result<(), Error> {
         with_timeout(self.delay.clone(), 5000, async {
             while self.read_byte().await? != 0xFF {}
@@ -151,7 +258,11 @@ where
         todo!()
     }
 
-    async fn write(&mut self, _block_address: u32, _data: &[[u8; SIZE]]) -> Result<(), Self::Error> {
+    async fn write(
+        &mut self,
+        _block_address: u32,
+        _data: &[[u8; SIZE]],
+    ) -> Result<(), Self::Error> {
         todo!()
     }
 
