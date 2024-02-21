1a2c699cf93ce07906506991864251291d999f90
Scott Mabin <scott@mabez.dev>
2024-02-21 00:37:07 +0000

    doc: explicitly state the byte and block units

--- block-device-driver/src/lib.rs
+++ block-device-driver/src/lib.rs
@@ -3,6 +3,12 @@
 
 /// A trait for a block devices
 ///
+/// The generic parameter `SIZE` on [BlockDevice] is the number of _bytes_ in a block 
+/// for this block device.
+///
+/// All addresses are zero indexed, and the unit is blocks. For example to read bytes
+/// from 1024 to 1536, the supplied block address would be 2.
+///
 /// This trait can be implemented multiple times to support various different block sizes.
 pub trait BlockDevice<const SIZE: usize> {
     type Error: core::fmt::Debug;
@@ -17,7 +23,7 @@ pub trait BlockDevice<const SIZE: usize> {
     /// Write one or more blocks at the given block address.
     async fn write(&mut self, block_address: u32, data: &[[u8; SIZE]]) -> Result<(), Self::Error>;
 
-    // Report the size of the block device.
+    // Report the size of the block device in bytes.
     async fn size(&mut self) -> Result<u64, Self::Error>;
 }
 
