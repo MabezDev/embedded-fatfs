7da33fc816d58d9801d6431c4056c7909a4a98cd
Scott Mabin <scott@mabez.dev>
2024-02-24 21:41:10 +0000

    clean up the embedded example a bit (#25)

--- examples/esp32c6/src/main.rs
+++ examples/esp32c6/src/main.rs
@@ -2,9 +2,11 @@
 #![no_main]
 
 use block_device_adapters::BufStream;
+use block_device_adapters::BufStreamError;
 use embassy_executor::Spawner;
 use embedded_fatfs::FsOptions;
-use embedded_io_async::{Read, Write};
+use embedded_hal_async::delay::DelayNs;
+use embedded_io_async::{Read, Seek, Write};
 use esp32c6_hal::{
     clock::ClockControl,
     dma::Dma,
@@ -19,6 +21,7 @@ use esp32c6_hal::{
     FlashSafeDma, IO,
 };
 use esp_backtrace as _;
+use sdspi::SdSpi;
 
 #[main]
 async fn main(_spawner: Spawner) {
@@ -59,70 +62,49 @@ async fn main(_spawner: Spawner) {
 
     let spi = FlashSafeDma::<_, 512>::new(spi);
 
-    let mut sd = sdspi::SdSpi::<_, _, _, aligned::A1>::new(
-        spi,
-        cs.into_push_pull_output(),
-        Delay(embassy_time::Delay),
-    );
+    let mut sd =
+        SdSpi::<_, _, _, aligned::A1>::new(spi, cs.into_push_pull_output(), embassy_time::Delay);
 
-    // Initialize the card
-    sd.init().await.unwrap();
+    loop {
+        // Initialize the card
+        if let Ok(_) = sd.init().await {
+            // Increase the speed up to the SD max of 25mhz
+            sd.spi()
+                .inner_mut()
+                .change_bus_frequency(25u32.MHz(), &clocks);
+            log::info!("Initialization complete!");
 
-    // Increase the speed up to the SD max of 25mhz
-    sd.spi()
-        .inner_mut()
-        .change_bus_frequency(25u32.MHz(), &clocks);
-
-    log::info!("Initialization complete!");
+            break;
+        }
+        log::info!("Failed to init card, retrying...");
+        embassy_time::Delay.delay_ns(5000u32).await;
+    }
 
     let inner = BufStream::<_, 512>::new(sd);
 
-    let fs = embedded_fatfs::FileSystem::new(inner, FsOptions::new())
-        .await
-        .unwrap();
+    async {
+        let fs = embedded_fatfs::FileSystem::new(inner, FsOptions::new()).await?;
+        {
+            let mut f = fs.root_dir().create_file("test.log").await?;
+            let hello = b"Hello world!";
+            log::info!("Writing to file...");
+            f.write_all(hello).await?;
+            f.flush().await?;
 
-    {
-        let mut f = fs.root_dir().create_file("test.log").await.unwrap();
-        let hello = b"Hello world!";
-        f.write_all(hello).await.unwrap();
-        f.flush().await.unwrap();
+            let mut buf = [0u8; 12];
+            f.rewind().await?;
+            f.read_exact(&mut buf[..]).await?;
+            log::info!(
+                "Read from file: {}",
+                core::str::from_utf8(&buf[..]).unwrap()
+            );
+        }
+        fs.unmount().await?;
+
+        Ok::<(), embedded_fatfs::Error<BufStreamError<sdspi::Error>>>(())
     }
-
-    // See https://github.com/MabezDev/embedded-fatfs/issues/19 for why this re open is currently needed
-    {
-        let mut f = fs.root_dir().open_file("test.log").await.unwrap();
-        let mut buf = [0u8; 12];
-        f.read_exact(&mut buf[..]).await.unwrap();
-        log::info!(
-            "Read from file: {}",
-            core::str::from_utf8(&buf[..]).unwrap()
-        );
-    }
-
-    fs.unmount().await.unwrap();
+    .await
+    .expect("Filesystem tests failed!");
 
     loop {}
-}
-
-// We can remove this once https://github.com/embassy-rs/embassy/pull/2593 is released
-pub struct Delay(embassy_time::Delay);
-
-impl Clone for Delay {
-    fn clone(&self) -> Self {
-        Self(embassy_time::Delay)
-    }
-}
-
-impl embedded_hal_async::delay::DelayNs for Delay {
-    async fn delay_ns(&mut self, ns: u32) {
-        embedded_hal_async::delay::DelayNs::delay_ns(&mut self.0, ns).await
-    }
-
-    async fn delay_us(&mut self, us: u32) {
-        embedded_hal_async::delay::DelayNs::delay_us(&mut self.0, us).await
-    }
-
-    async fn delay_ms(&mut self, ms: u32) {
-        embedded_hal_async::delay::DelayNs::delay_ms(&mut self.0, ms).await
-    }
 }
