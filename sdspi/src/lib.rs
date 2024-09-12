4bfe7b12a2a6b40233dade12fab3559dd392ad88
Caleb Jamison <caleb@cbjamo.com>
2024-09-12 06:18:17 -0400

    Move clock init to free func, update examples (#30)
    * Move clock init to free func, update examples
    
    * Fixup
    
    Add build profiles to 2040 example so defmt works right
    Remove unnecessary deps from 2040 example
    
    * Make sure CS is deasserted for init.

--- sdspi/src/lib.rs
+++ sdspi/src/lib.rs
@@ -69,6 +69,19 @@ pub enum Error {
     WriteError,
 }
 
+/// Must be called between powerup and [SdSpi::init] to ensure the sdcard is properly initialized.
+pub async fn sd_init<SPI, CS, BE>(spi: &mut SPI, cs: &mut CS) -> Result<(), Error>
+where
+    SPI: embedded_hal_async::spi::SpiBus<Error = BE>,
+    CS: embedded_hal::digital::OutputPin,
+{
+    // Supply minimum of 74 clock cycles without CS asserted.
+    cs.set_high().map_err(|_| Error::ChipSelect)?;
+    spi.write(&[0xFF; 10]).await.map_err(|_| Error::SpiError)?;
+
+    Ok(())
+}
+
 pub struct SdSpi<SPI, D, ALIGN>
 where
     SPI: embedded_hal_async::spi::SpiDevice,
@@ -96,14 +109,9 @@ where
         }
     }
 
+    /// To comply with the SD card spec, [sd_init] must be called between powerup and calling this function.
     pub async fn init(&mut self) -> Result<(), Error> {
         let r = async {
-            // Supply minimum of 74 clock cycles without CS asserted.
-            self.spi
-                .write(&[0xFF; 10])
-                .await
-                .map_err(|_| Error::SpiError)?;
-
             with_timeout(self.delay.clone(), 1000, async {
                 loop {
                     let r = self.cmd(idle()).await?;
