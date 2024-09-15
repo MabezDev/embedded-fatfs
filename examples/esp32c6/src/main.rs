5ac34086222eaad1d36e91a349369e4188a2367f
Caleb Jamison <caleb@cbjamo.com>
2024-09-15 16:29:40 -0400

    Update esp32c6 example

--- examples/esp32c6/src/main.rs
+++ examples/esp32c6/src/main.rs
@@ -11,62 +11,58 @@ use embedded_io_async::{Read, Seek, Write};
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    dma::Dma,
-    dma::DmaPriority,
-    dma_descriptors, embassy,
+    dma::*,
+    dma_buffers,
+    gpio::{Io, Level, Output},
     peripherals::Peripherals,
     prelude::*,
     spi::{
-        master::{prelude::*, Spi},
+        master::{Spi, SpiDmaBus},
         SpiMode,
     },
-    FlashSafeDma, IO,
+    system::SystemControl,
+    timer::timg::TimerGroup,
 };
 use sdspi::{self, SdSpi};
 
-#[main]
+#[esp_hal_embassy::main]
 async fn main(_spawner: Spawner) {
     let peripherals = Peripherals::take();
-    let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::max(system.clock_control).freeze();
+    let system = SystemControl::new(peripherals.SYSTEM);
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    embassy::init(
-        &clocks,
-        esp_hal::systimer::SystemTimer::new(peripherals.SYSTIMER),
-    );
+    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    esp_hal_embassy::init(&clocks, timg0.timer0);
 
     esp_println::logger::init_logger_from_env();
     log::info!("Hello world!");
 
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     let sclk = io.pins.gpio18;
     let miso = io.pins.gpio20;
     let mosi = io.pins.gpio19;
-    let cs = io.pins.gpio9;
+    let mut cs = Output::new(io.pins.gpio9, Level::High);
 
     let dma = Dma::new(peripherals.DMA);
     let dma_channel = dma.channel0;
 
-    let (mut descriptors, mut rx_descriptors) = dma_descriptors!(32000);
+    let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(32000);
+    let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+    let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
 
     // Initialize spi at the maxiumum SD initialization frequency of 400 khz
     let spi = Spi::new(peripherals.SPI2, 400u32.kHz(), SpiMode::Mode0, &clocks)
         .with_sck(sclk)
         .with_miso(miso)
         .with_mosi(mosi)
-        .with_dma(dma_channel.configure(
-            false,
-            &mut descriptors,
-            &mut rx_descriptors,
-            DmaPriority::Priority0,
-        ));
+        .with_dma(dma_channel.configure_for_async(false, DmaPriority::Priority0));
 
-    let mut spi = FlashSafeDma::<_, 512>::new(spi);
+    let mut spi = SpiDmaBus::new(spi, dma_tx_buf, dma_rx_buf);
 
     // Sd cards need to be clocked with a at least 74 cycles on their spi clock without the cs enabled,
     // sd_init is a helper function that does this for us.
     loop {
-        match sdspi::sd_init(&mut spi).await {
+        match sdspi::sd_init(&mut spi, &mut cs).await {
             Ok(_) => break,
             Err(e) => {
                 log::warn!("Sd init error: {:?}", e);
@@ -75,16 +71,15 @@ async fn main(_spawner: Spawner) {
         }
     }
 
-    let spid = ExclusiveDevice::new(spi, cs.into_push_pull_output(), embassy_time::Delay);
+    let spid = ExclusiveDevice::new(spi, cs, embassy_time::Delay);
     let mut sd = SdSpi::<_, _, aligned::A1>::new(spid, embassy_time::Delay);
 
     loop {
         // Initialize the card
-        if let Ok(_) = sd.init().await {
+        if sd.init().await.is_ok() {
             // Increase the speed up to the SD max of 25mhz
             sd.spi()
                 .bus_mut()
-                .inner_mut()
                 .change_bus_frequency(25u32.MHz(), &clocks);
             log::info!("Initialization complete!");
 
